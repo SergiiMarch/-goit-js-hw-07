1e8ef6b4d60d7e39a1d1eae224da776268a77fd6
Sergii Marchenko <marikmarchelo@gmail.com>
2023-05-20 17:00:10 +0300

    зробив дз 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEls = document.querySelector(".gallery");
+
+const createImageItem = ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>    
+    `;
+};
+
+const arrImages = galleryItems.map(createImageItem).join("");
+galleryEls.insertAdjacentHTML("beforeend", arrImages);
+console.log(galleryEls);
+
+galleryEls.addEventListener("click", (event) => {
+  event.preventDefault();
+  gallery.on("show.Simplelightbox");
+});
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
